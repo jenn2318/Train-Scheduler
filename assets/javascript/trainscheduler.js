e8c05ce7006f8a3e5ec6826239554a90c106a4b9
Jennifer Ponder <jponder2000@yahoo.com>
2018-03-27 16:46:02 -0400

    wipupdated

--- assets/javascript/trainscheduler.js
+++ assets/javascript/trainscheduler.js
@@ -1,13 +1,10 @@
 //===================================================================================
 //Global Variables I will need for functions later
 //===================================================================================
-var currentTime = moment.format('HH:mm');
-var childData = snapshot.val();
-var trainName = '';
-var Destination = '';
-var trainFrequency = '';
-var nextArrival = '';
-var minutes Away = '';
+// var currentTime = moment.format('HH:mm');
+// var childData = snapshot.val();
+// var nextArrival = '';
+// var minutes Away = '';
 
 //Use to access Firebase below:
 //Initialize Firebase
@@ -20,3 +17,36 @@ var config = {
     storageBucket: "jennp-6f180.appspot.com",
     messagingSenderId: "911300040236"
   };
+
+  firebase.initializeApp(config);
+
+  var trainDataInfo = firebase.database();
+
+  //First thing will be to add train information
+  //===================================================================
+
+  $("#add-train-button ").on("click", function () {
+  	   var trainName = $("#train-Name").val();
+  	   var destination = $("#destination").val();
+  	   var firstTrainTime = $("#first-train-time").val();
+  	   var frequency = $("#frequency").val();
+  
+
+    var newTrain = {
+    	name: trainName,
+    	destination: destination,
+    	firstTrain: firstTrainTime,
+    	frequency: frequency
+    };
+     
+ //Uploads train information to the database
+   trainDataInfo.ref().push(newTrain);
+
+    //Console logs to check to see if everything shows up
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrainTime);
+    console.log(newTrain.frequency);
+    alert("Train has been added successfully!");
+
+});
